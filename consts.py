094d097527a8c5a92a2439de5d8e6bfc97936cf5
Noam <Noam347levi@gmail.com>
2025-08-24 15:35:06 +0300

    screen

--- consts.py
+++ consts.py
@@ -1,9 +1,17 @@
 GRID_ROWS = 25
 GRID_COLUMNS = 50
+
+BACKGROUND_COLOR = (0, 128, 0)
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 1000
+
+
+
 SOLDIER_IMG = "soldier.png"
-SOLDIER_NIGTH_IMG = "soldier_night.png"
+SOLDIER_NIGTH_IMG = "soldier_nigth.png"
 EXPLOTION_IMG = "explotion.png"
 MINE_IMG = "mine.png"
 GRASS_IMG = "grass.png"
 FLAG_IMG = "flag.png"
+
 
