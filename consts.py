fff70d86cb9816176ecabd952d7acf9bf22f945a
Noam <Noam347levi@gmail.com>
2025-08-24 11:35:26 +0300

    conts

--- consts.py
+++ consts.py
@@ -1,1 +1,10 @@
+GRID_ROWS = 25
+GRID_COLUMNS = 50
+
+SOLDIER_IMG = "soldier.png"
+SOLDIER_NIGTH_IMG = "soldier_night.png"
+EXPLOTION_IMG = "explotion.png"
+MINE_IMG = "mine.png"
+GRASS_IMG = "grass.png"
+FLAG_IMG = "flag.png"
 
