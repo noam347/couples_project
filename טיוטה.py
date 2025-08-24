84a6e72d086e44229d72e059e109b3b131d4ba63
Noam <Noam347levi@gmail.com>
2025-08-24 15:41:26 +0300

    screen

--- טיוטה.py
+++ טיוטה.py
@@ -1,22 +1,9 @@
-import pygame
-import consts
-
 BG = (4, 10, 2)
 GREEN = (25, 68, 34)
 tile_size = 20
+
 soldier = pygame.image.load(consts.SOLDIER_NIGTH_IMG)
 mine = pygame.image.load(consts.MINE_IMG)
-# x_ray_screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
-# x_ray_screen.fill(BG)
-# for y in range(0, consts.GRID_ROWS * 20, tile_size):
-#     pygame.draw.lines(x_ray_screen, GREEN, True, ((0, y), (consts.WINDOW_WIDTH, y)))
-#
-# for x in range(0, consts.GRID_COLUMNS * 20, tile_size):
-#     pygame.draw.lines(x_ray_screen, GREEN, True, ((x, 0), (x, consts.WINDOW_HEIGHT)))
-#
-# soldier = pygame.image.load(consts.SOLDIER_NIGTH_IMG)
-# x_ray_screen.blit(soldier, (0,0))
-
 
 def create_x_ray_board(real_matrix,soldier_location):
     x_ray_screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
